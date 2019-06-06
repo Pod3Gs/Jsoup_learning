a159fc9c988419265e7165c1828e574ce885cbfe
Pod3Gs <sc1275622236@sina.cn>
2019-06-06 16:22:53 +0800

    1、改为使用相对路径 2、对代码使用google-java-format格式化

--- src/main/java/com/scott/jsoup/Demo01.java
+++ src/main/java/com/scott/jsoup/Demo01.java
@@ -19,63 +19,61 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Demo01 {
-    public static void main(String[] args) throws IOException {
-        //利用httpclient爬数据
-        String url = "http://www.hao123.com";
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpGet httpGet = new HttpGet(url);
-        CloseableHttpResponse response = null;
-        response = httpClient.execute(httpGet);
-        HttpEntity entity = response.getEntity();
-        String content = EntityUtils.toString(entity,"utf-8");
-        response.close();
+  public static void main(String[] args) throws IOException {
+    // 利用httpclient爬数据
+    String url = "http://www.hao123.com";
+    CloseableHttpClient httpClient = HttpClients.createDefault();
+    HttpGet httpGet = new HttpGet(url);
+    CloseableHttpResponse response = httpClient.execute(httpGet);
+    HttpEntity entity = response.getEntity();
+    String content = EntityUtils.toString(entity, "utf-8");
+    response.close();
 
-        //利用Joup处理收到的数据
-        Document doc = Jsoup.parse(content);
-        Elements elements = doc.getElementsByTag("a");
-        List<String> strings = new ArrayList<String>();
-        for (int i = 0; i < elements.size(); i++) {
-            Element element = elements.get(i);
-            String hrefGotten = element.toString();
-            strings.add(getAHref(hrefGotten));
-        }
-        System.out.println(strings);
-
-        //将所有的超链接输出
-        String filePath = "/Users/wangjiatao/Downloads/Jsou_learning/src/main/resources/file.txt";
-        FileWriter clearFile = new FileWriter(filePath);
-        clearFile.write("");
-        clearFile.close();
-        FileWriter fw = new FileWriter(filePath,true);
-        fw.write("-------------------来自"+url+"的超链接---------------"+"\n");
-        for (int i = 0; i < strings.size(); i++) {
-            String a = "\"#\"";
-            String b = "";
-            String c = "\"javascript:;\"";//"javascript:;"
-
-            if (!strings.get(i).equals(a) && !strings.get(i).equals(b) && !strings.get(i).equals(c))
-            fw.write(strings.get(i)+"\n");
-        }
-        fw.close();
-
+    // 利用Joup处理收到的数据
+    Document doc = Jsoup.parse(content);
+    Elements elements = doc.getElementsByTag("a");
+    List<String> strings = new ArrayList<String>();
+    for (Element element : elements) {
+      String hrefGotten = element.toString();
+      strings.add(getAHref(hrefGotten));
     }
+    System.out.println(strings);
 
-    public static String getAHref(String strs){
-        String result = new String();
-        String regex="<a.*?/a>";
+    // 将所有的超链接输出
+    String filePath = Demo01.class.getClassLoader().getResource("file.text").getPath();
+    //String filePath = "src/main/resources/file.txt";
+    FileWriter clearFile = new FileWriter(filePath);
+    clearFile.write("");
+    clearFile.close();
+    FileWriter fw = new FileWriter(filePath, true);
+    fw.write("-------------------来自" + url + "的超链接---------------" + "\n");
+    for (String string : strings) {
+      String a = "\"#\"";
+      String b = "";
+      String c = "\"javascript:;\""; // "javascript:;"
 
-        Pattern pt=Pattern.compile(regex);
-        Matcher mt=pt.matcher(strs);
-
-        while(mt.find()){
-            String s3 = "href=\"(.*?)\"";
-            Pattern pt3=Pattern.compile(s3);
-            Matcher mt3=pt3.matcher(mt.group());
-            while(mt3.find())
-            {
-                result=mt3.group().replaceAll("href=|>","");
-            }
-        }
-        return result;
+      if (!string.equals(a) && !string.equals(b) && !string.equals(c)) {
+        fw.write(string + "\n");
+      }
     }
+    fw.close();
+  }
+
+  private static String getAHref(String strs) {
+    String result = "";
+    String regex = "<a.*?/a>";
+
+    Pattern pt = Pattern.compile(regex);
+    Matcher mt = pt.matcher(strs);
+
+    while (mt.find()) {
+      String s3 = "href=\"(.*?)\"";
+      Pattern pt3 = Pattern.compile(s3);
+      Matcher mt3 = pt3.matcher(mt.group());
+      while (mt3.find()) {
+        result = mt3.group().replaceAll("href=|>", "");
+      }
+    }
+    return result;
+  }
 }
